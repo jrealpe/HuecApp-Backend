f383938b0cbd63ae1657cdf1d3b31f08d5c9a923
Manuel Suárez <man.srz@gmail.com>
2015-06-06 14:11:22 -0500

    Add models

--- web/models.py
+++ web/models.py
@@ -1,3 +1,50 @@
-from django.db import models
+# -*- coding: utf-8 -*-
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=64)
+    place = models.CharField(max_length=128)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+   
+    class Meta:
+        verbose_name = 'Restaurante'
+        verbose_name_plural = 'Restaurantes'
+
+class Text(models.Model):
+    name = models.CharField(max_length=128)
+
+    class Meta:
+        abstract = True
+
+class Dish(Text):
+    restaurant = models.ManyToManyField(Restaurant, through = 'RestaurantDish', through_fields = ('dish','restaurant'))
+
+
+
+class RestaurantDish(models.Model):
+    restaurant = models.ForeignKey(Restaurant)
+    dish = models.ForeignKey(Dish)
+    price = models.DecimalField(max_digits = 10, decimal_places = 3)
+    
+
+
+class Category(Text):
+    pass
+
+class Evaluation(models.Model):
+    user = models.ForeignKey(User, related_name = 'evaluations')
+    restaurantdish = models.ForeignKey(RestaurantDish, related_name = 'evaluations')
+    category = models.ForeignKey(Category, related_name = 'evaluations')
+    evaluation = models.PositiveSmallIntegerField()
+
+
+
+    
+
+
+

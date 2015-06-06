17780a245e3535de84e93d74723a39812d74c46e
Manuel Suárez <man.srz@gmail.com>
2015-06-06 14:11:37 -0500

    Including models in admin

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from models import Restaurant, Dish, RestaurantDish, Category, Evaluation
+
 # Register your models here.
+admin.site.register(Restaurant)
+admin.site.register(Dish)
+admin.site.register(RestaurantDish)
+admin.site.register(Category)
+admin.site.register(Evaluation)

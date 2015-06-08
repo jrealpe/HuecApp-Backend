ce62afa3e2d5739be07a53d7586c6e9d521d6e00
mansrz <man.srz@gmail.com>
2015-06-08 04:33:57 -0500

    Add views for login and logout

--- web/views.py
+++ web/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
-
+from django.template import RequestContext
+import json
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import never_cache
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseBadRequest, HttpResponseForbidden
+f
 # Create your views here.
+
+
+@never_cache
+def login(request):
+    #if not request.is_ajax() or request.method != 'GET':
+    #    return
+    try:
+        username = request.POST['username']
+        password = request.POST['password']
+    except:
+        return HttpResponseBadRequest('Bad parameters')
+
+    from django.contrib.auth import authenticate, login
+
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+
+            response_content = {
+                'username': user.username,
+            }
+            response =  HttpResponse(json.dumps(response_content))
+            response['Content-Type'] = 'application/json; charset=utf-8'
+            response['Cache-Control'] = 'no-cache'
+            return response
+        else:
+            # Return a 'disabled account' error message
+            return HttpResponseBadRequest('Usuario ha sido supendido')
+    else:
+        # Return an 'invalid login' error message.
+         return HttpResponseBadRequest('Usuario o contraseña incorrecto')
+
+         
+@never_cache
+def logout(request):
+    from django.contrib.auth import logout
+    logout(request)
+    return redirect('/')
+
+

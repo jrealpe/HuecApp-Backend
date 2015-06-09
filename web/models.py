c083c345c95f5755297e4e0dc64f2c4d9cb80750
mansrz <man.srz@gmail.com>
2015-06-08 22:02:02 -0500

    Add filters and concising data

--- web/models.py
+++ web/models.py
@@ -5,34 +5,38 @@ from django.contrib.auth.models import User
 
 
 class Restaurant(models.Model):
-    name = models.CharField(max_length=64)
-    place = models.CharField(max_length=128)
-    latitude = models.FloatField()
-    longitude = models.FloatField()
-
-   
+    name = models.CharField(max_length = 64)
+    place = models.CharField(max_length = 128)
+    latitude = models.FloatField(null = True)
+    longitude = models.FloatField(null = True)
+  
+    def __unicode__(self):
+        return self.name
+  
     class Meta:
         verbose_name = 'Restaurante'
         verbose_name_plural = 'Restaurantes'
 
 class Text(models.Model):
-    name = models.CharField(max_length=128)
+    name = models.CharField(max_length = 128)
 
+    def __unicode__(self):
+        return self.name
+ 
     class Meta:
         abstract = True
 
 class Dish(Text):
     restaurant = models.ManyToManyField(Restaurant, through = 'RestaurantDish', through_fields = ('dish','restaurant'))
 
-
-
 class RestaurantDish(models.Model):
     restaurant = models.ForeignKey(Restaurant)
     dish = models.ForeignKey(Dish)
     price = models.DecimalField(max_digits = 10, decimal_places = 3)
-    
 
-
+    def __unicode__(self):
+        return self.dish.name
+ 
 class Category(Text):
     pass
 
--- web/viewsets.py
+++ web/viewsets.py
@@ -17,6 +17,13 @@ class RestaurantDishViewSet(viewsets.ModelViewSet):
     serializer_class = RestaurantDishSerializer
     queryset = RestaurantDish.objects.all()
 
+    def get_queryset(self):
+        queryset = RestaurantDish.objects.all()
+        restaurant = self.request.query_params.get('restaurant', None)
+        if restaurant is not None:
+            queryset = queryset.filter(restaurant=restaurant)
+        return queryset
+
 class CategoryViewSet(viewsets.ModelViewSet):
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
@@ -25,4 +32,13 @@ class EvaluationViewSet(viewsets.ModelViewSet):
     serializer_class = EvaluationSerializer
     queryset = Evaluation.objects.all()
 
+    def get_queryset(self):
+        queryset = Evaluation.objects.all()
+        restaurantdish = self.request.query_params.get('restaurantdish', None)
+        user = self.request.query_params.get('username', None)
+        if user is not None:
+            queryset = queryset.filter(user__username = username)
+        if restaurantdish is not None:
+            queryset = queryset.filter(restaurantdish = restaurantdish)
+        return queryset
 
